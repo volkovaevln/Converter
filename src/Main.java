292e9936d0734e84f517cafac42fde98249bde5d
volkovaevln <volkova.evln@gmail.com>
2026-07-23 00:16:28 +0300

    Add miles to kilometers

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-import java.util.SortedMap;
-
 public class Main {
+    private static final double MILES_TO_KM = 1.60934;
+
     public static void main(String[] args){
         System.out.println("A program for converting miles to kilometers and kilometers to miles.");
+
+        double miles = 10;
+        double kilometers = convertMilesToKilometers(miles);
+
+        System.out.println(miles + " miles = " + kilometers + " kilometers");
+    }
+
+    private static double convertMilesToKilometers(double miles){
+        return miles * MILES_TO_KM;
     }
 }
