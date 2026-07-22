1d29822450394689a7ade221ca1c22d505f7df1e
volkovaevln <volkova.evln@gmail.com>
2026-07-22 23:59:38 +0300

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.util.SortedMap;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("A program for converting miles to kilometers and kilometers to miles.");
+    }
+}
